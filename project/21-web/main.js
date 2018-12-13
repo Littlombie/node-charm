a83f76b0550bc435d5129e7123197c56b35b9cc6
Littlombie <deanwen258@outlook.com>
2018-12-13 23:58:22 +0800

    updated node web

---
+++ project/21-web/main.js
@@ -0,0 +1,31 @@
+var http  = require('http');
+var fs = require('fs');
+var url = require('url');
+
+// 创建web服务器
+http.createServer( function (request, response) {
+  // 解析请求，包括文件名
+  var platname = url.parse(request.url).pathname;
+  // 输出请求的文件名
+  console.log('Request for' + platname + 'received.');
+  // 从文件系统中读取请求的文件内容
+  fs.readFile(platname.substr(1), function (err, data) {
+    if (err) {
+      console.log(err);
+      // HTTP 状态码： 404： NOT FOUND
+      //Content Type: text/plain
+      response.writeHead(404, {'Content-Type': 'text/html'});
+    } else {
+      // HTTp 状态码： 200 ： OK
+      // Content Type: text/plain
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      // 响应文件内容
+      response.write(data.toString());
+    }
+    // 发送相应数据
+     response.end();
+  });
+}).listen(8080);
+
+// 控制台会输出以下信息
+console.log('Server running at http://127.0.0.1:8080/');
