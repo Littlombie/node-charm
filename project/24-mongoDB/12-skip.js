dfc2ca79d83f2cc0913e31fdc68ca822e97a8b14
Littlombie <Littlombie@users.noreply.github.com>
2018-12-19 19:34:43 +0800

    Create 12-skip.js

---
+++ project/24-mongoDB/12-skip.js
@@ -0,0 +1,14 @@
+// 查询 如果要指定跳过的条数，可以使用 skip() 方法。
+
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/';
+
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db('runoob');
+    dbo.collection('site').find().skip(2).limit(2).toArray(function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        db.close();
+    })
+})
