7fb7bb5bc96441d588e304d211d94d782017ea72
Littlombie <deanwen258@outlook.com>
2019-02-11 23:34:15 +0800

    updated

--- client/app.js
+++ client/app.js
@@ -8,8 +8,9 @@ const fs = require('fs');
 
 const path = require('path');
 
-import axios from '/node_modules/axios/dist/axios.js';
-import login from './public/js/login.js'
+const axios = require('axios');
+
+// import login from '/js/login.js';
 
 console.log( path.join(__dirname, 'public'))
 /* 
