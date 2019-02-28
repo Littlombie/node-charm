89f6e2285a1e1f9ed5b6e1b8536162fea9d1403d
Littlombie <deanwen258@outlook.com>
2019-03-01 00:04:57 +0800

    updated

---
+++ server/route/static.js
@@ -0,0 +1,41 @@
+var fs = require('fs');
+var path = require('path');
+
+var MIME = {};
+MIME['.js'] = 'text/js';
+MIME['.css'] = 'text/css';
+MIME['.jpg'] = 'image/jpg';
+MIME['.png'] = 'image/png';
+MIME['.gif'] = 'image/gif';
+MIME['.svg'] = 'image/svg';
+
+function get (pathname, res) {
+  if (fs.existsSync(pathname)) { // 检测目录是否存在
+    var extname = path.extname(pathname);
+    res.wirteHead(200, {'Content-type': extname});
+    fs.readFile(pathname, (req, res) => {
+      if (err) {
+        console.log(err);
+        res.end();
+      } else {
+        if (isImage(extname)) {
+          res.end(data, 'binary'); // 二进制文件加上 binary
+        } else {
+          res.end(data.toString());
+        }
+      }
+    });
+  }
+}
+
+function  isImage(extname) {
+  if (extname === '.jpg' || extname === '.png' || extname === '.gif' || extname === 'svg') {
+    return true;
+  } 
+  return false;
+}
+
+// 提供给其他模块使用的接口
+module.exports = {
+  get: get
+}
