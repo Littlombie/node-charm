4d6e24dc3fe95665e6da0253e320e495cc31a617
Littlombie <Littlombie@users.noreply.github.com>
2019-02-28 17:48:39 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,6 +6,15 @@ var static = require('./router/static');
 // 匹配静态文件夹路径的正则表达式，用于判断请求是否是静态文件
 var staticExp = /\/public\/(img/js/css)\/[a-z]*\.(jpg|png|gif|svg|js|css)/;
 
+http.createServer((req, res) => {
+    var pathname = url.parse(req.url).pathname;
+    if (staticExp.test(pathname)) { // 静态文件请求交由static处理
+        static.get(__dirname + pathname, res);
+    } else if (req.method == 'POST') { // 处理普通post请求
+        api.post(req, res);
+    } else { //普通请求
+        api.get(req, res);
+    }
+}).listen(8520);
 
-
-// https://e.huawei.com/cn/products/enterprise-networking/~/media/EBG/topic-minisite/sdc/style.css
+console.log('[Server Info] start server at http://localhost:8520');
